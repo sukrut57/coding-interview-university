ef4962be0ae711f2314f78288fa411498aa71c56
sukrut kesari <sukrutkesari@gmail.com>
2022-02-12 22:29:59 -0500

    started big o notation

---
+++ conceptsByCode/ArraySorting/BubbleSort.java
@@ -0,0 +1,22 @@
+package conceptsByCode.ArraySorting;
+
+public class BubbleSort{
+    public static void main(String[] args){
+        BubbleSort bubbleSort=new BubbleSort();
+        bubbleSort.bubbleSort(new int[] {1,2,4,5,6});
+    }
+
+    public void bubbleSort(int[] arr){
+        int arraySize=arr.length;
+        for(int i=arraySize-1;i>0;i--){
+            for(int j=0;j<i;j++){
+                if(arr[j] > arr[j+1]){
+                    int temp=arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int i:arr) System.out.println(i);
+    }
+}   
